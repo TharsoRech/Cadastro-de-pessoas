8e0975cf2af5b246e741267cd3d90e50fc23793c
Tharso Rech <77845449+TharsoRech@users.noreply.github.com>
2022-01-05 15:23:49 -0300

    Revert "PetShopCriando"
    This reverts commit aafa3a2480d89135d4996313194dd58aa3962e09.

---
+++ Cadastro de pessoas/Data/AppliccationContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cadastro_de_pessoas.Data
+{
+    public class AppliccationContext: DbContext
+    {
+
+        public DbSet<Cadastro_de_pessoas.Models.Pessoa> pessoas { get; set; }
+
+        public AppliccationContext(DbContextOptions<AppliccationContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=master;Trusted_Connection=True;");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cadastro_de_pessoas.Models.Pessoa>(p =>
+                {
+                    p.ToTable("Pessoas");
+                    p.Property(p => p.Id).ValueGeneratedOnAdd();
+                    p.Property(p => p.Nome).HasColumnType("VARCHAR(80)").IsRequired();
+                    p.Property(p => p.DataDeNascimento).HasColumnType("VARCHAR(80)").IsRequired();
+                    p.Property(p => p.Ativo).HasColumnType("VARCHAR(80)");
+                });
+            
+        }
+    }
+}
--- MeuPetShop/Models/Genero.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Cadastro_de_pessoas.Models
-{
-    public enum Genero
-    {
-        Masculino=1,
-        Feminino=2
-    }
-    
-}
